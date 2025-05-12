7618c1c1b6152ca4f178f63a73fc47e430444ca7
Pedro Cezarini <pedrocezarini13@gmail.com>
2025-05-12 15:14:21 -0300

    usando binding pra alterar um TextView via código

--- app/src/main/java/com/example/gasapp/MainActivity.kt
+++ app/src/main/java/com/example/gasapp/MainActivity.kt
@@ -8,16 +8,14 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.gasapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        var binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
-    }
+        binding.textResposta.text = "Saide de texto via codigo"
+        }
 }
